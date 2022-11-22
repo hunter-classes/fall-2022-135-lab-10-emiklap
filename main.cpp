96afc11582b3441edf9e430ee6290bf87afff5ac
Emily Klapper <emiklap@gmail.com>
2022-11-21 20:40:13 -0500

    updated task C demo

--- main.cpp
+++ main.cpp
@@ -24,12 +24,14 @@ int main() {
     Movie saving_face = {"Saving Face", ROMANCE, 91};
     TimeSlot morning_eeaao = {eeaao, {9, 15}};
     TimeSlot afternoon_eeaao = {eeaao, {12, 00}};
+    TimeSlot evening_eeaao = {eeaao, {16, 50}};
     TimeSlot afternoon_sf = {saving_face, {12, 30}};
-    TimeSlot evening_sf = {saving_face, {4, 45}};
+    TimeSlot evening_sf = {saving_face, {16, 45}};
 
     std::cout << "\n\n---------------------------" << "\n     DEMO OF TASK C" << std::endl;
     std::cout << getTimeSlot(morning_eeaao) << std::endl;
     std::cout << getTimeSlot(afternoon_eeaao) << std::endl;
+    std::cout << getTimeSlot(evening_eeaao) << std::endl;
     std::cout << getTimeSlot(afternoon_sf) << std::endl;
     std::cout << getTimeSlot(evening_sf) << std::endl;
 
