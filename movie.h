33ce7d779ccb68c4da470d6f33b77304fc9e10b8
Emily Klapper <emiklap@gmail.com>
2022-11-25 17:40:03 -0500

    added == operator

--- movie.h
+++ movie.h
@@ -7,4 +7,7 @@ public:
     std::string title;
     Genre genre;     // only one genre per movie
     int duration;    // in minutes
+    bool operator==(const Movie& new_movie) const {
+        return title == new_movie.title && genre == new_movie.genre && duration == new_movie.duration;
+    }
 };
--- timeslot.h
+++ timeslot.h
@@ -6,4 +6,7 @@ class TimeSlot {
 public:
     Movie movie;     // what movie
     Time startTime;  // when it starts
+    bool operator==(const TimeSlot& new_timeSlot) const {
+        return movie == new_timeSlot.movie && startTime == new_timeSlot.startTime;
+    }
 };
